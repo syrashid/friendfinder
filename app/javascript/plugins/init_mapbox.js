36d49dbae43051da4acf4c89a47914cf00a2d452
Sy Rashid <sy.rashid.11@gmail.com>
2020-06-03 10:45:53 +1000

    FEAT: Added in cool mapbox stylings

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,4 +1,5 @@
 import mapboxgl from 'mapbox-gl';
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
@@ -6,13 +7,28 @@ const fitMapToMarkers = (map, markers) => {
   map.fitBounds(bounds, { padding: 100, maxZoom: 15, duration: 5000 });
 };
 
-const addMarkersToMap = (map, markers) => {
-    markers.forEach((marker) => {
-    new mapboxgl.Marker()
-    .setLngLat([ marker.lng, marker.lat ])
-    .addTo(map);
-  });
+const createElement = (marker) => {
+  const element = document.createElement('div');
+  element.className = 'marker';
+  element.style.backgroundImage = `url('${marker.image_url}')`;
+  element.style.backgroundSize = 'contain';
+  element.style.width = '50px';
+  element.style.height = '50px';
+  return element;
 }
+
+const addMarkersToMap = (map, markers) => {
+  markers.forEach((marker) => {
+    const popup = new mapboxgl.Popup().setHTML(marker.infoWindow); // add this
+
+    new mapboxgl.Marker(createElement(marker))
+    .setLngLat([ marker.lng, marker.lat ])
+      .setPopup(popup) // add this
+      .addTo(map);
+    });
+};
+
+
 
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
@@ -21,12 +37,15 @@ const initMapbox = () => {
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
   const map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v10'
+    style: 'mapbox://styles/syrashid11/ckay160oc15ss1ili7611upxn'
   });
   const markers = JSON.parse(mapElement.dataset.markers);
   addMarkersToMap(map, markers);
   fitMapToMarkers(map, markers);
-}
+  map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
+                                      mapboxgl: mapboxgl }));
+  }
+
 };
 
 export { initMapbox };
